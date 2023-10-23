0942ff4f8a538d51a2db109feb62c9512ee7b898
l-muller <lisi_muller@hotmail.com>
2023-10-23 02:29:36 -0300

    Incluindo camadas no chapter 1

--- config.js
+++ config.js
@@ -3,9 +3,7 @@ var config = {
     accessToken: 'pk.eyJ1IjoiY29udmlkYWRvIiwiYSI6ImNqZ2wzYWVuMDE2cm8zM284cm4wZ25uc2wifQ.d8Y_NpxazV3JB68v7qmGtg',
     showMarkers: false,
     markerColor: '#3FB1CE',
-    //projection: 'equirectangular',
-    //Read more about available projections here
-    //https://docs.mapbox.com/mapbox-gl-js/example/projections/
+    projection: 'mercator',
     inset: true,
     theme: 'light',
     use3dTerrain: false, //set true for enabling 3D maps.
@@ -24,24 +22,103 @@ var config = {
             description: 'Test Chapter 1',
             location: {
                 center: [-59.19000, -5.93000],
-                zoom: 4.11,
-                pitch: 60,
-                bearing: 0
+                zoom: 4,
+                pitch: 60.00,
+                bearing: 0.00
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
             callback: '',
             onChapterEnter: [
                 {
+                    layer: 'Satellite imagery',
+                    opacity: 1
+                },
+                {
+                    layer: 'Road network tunnels',
+                    opacity: 1
+                },
+                {
+                    layer: 'Road network surface',
+                    opacity: 1
+                },
+                {
+                    layer: 'Road network bridges',
+                    opacity: 1
+                },
+                {
+                    layer: 'Administrative boundaries',
+                    opacity: 1
+                },
+                {
+                    layer: 'Road network',
+                    opacity: 1
+                },
+                {
+                    layer: 'Natural features',
+                    opacity: 1
+                },
+                {
+                    layer: 'Point of interest labels',
+                    opacity: 1
+                },
+                {
+                    layer: 'Place labels',
+                    opacity: 1
+                },
+                {
                     layer: 'states-legal-amazon',
-                    opacity: 1,
-                    duration: 5000
+                    opacity: 1
+                },
+                {
+                    layer: 'sitios_areas_florestais',
+                    opacity: 1
                 }
             ],
             onChapterExit: [
                 {
+                    layer: 'Satellite imagery',
+                    opacity: 1
+                },
+                {
+                    layer: 'Road network tunnels',
+                    opacity: 1
+                },
+                {
+                    layer: 'Road network surface',
+                    opacity: 1
+                },
+                {
+                    layer: 'Road network bridges',
+                    opacity: 1
+                },
+                {
+                    layer: 'Administrative boundaries',
+                    opacity: 1
+                },
+                {
+                    layer: 'Road network',
+                    opacity: 1
+                },
+                {
+                    layer: 'Natural features',
+                    opacity: 1
+                },
+                {
+                    layer: 'Point of interest labels',
+                    opacity: 1
+                },
+                {
+                    layer: 'Place labels',
+                    opacity: 1
+                },
+                {
                     layer: 'states-legal-amazon',
-                    opacity: 1,
+                    opacity: 1
+                },
+                {
+                    layer: 'sitios_areas_florestais',
+                    opacity:1
                 }
             ]
         },
@@ -54,9 +131,9 @@ var config = {
             description: 'Copy these sections to add to your story.',
             location: {
                 center: [-59.19000, -5.93000],
-                zoom: 4.11,
-                pitch: 60,
-                bearing: 0
+                zoom: 4,
+                pitch: 60.00,
+                bearing: 0.00
                 // flyTo additional controls-
                 // These options control the flight curve, making it move
                 // slowly and zoom out almost completely before starting
@@ -79,9 +156,9 @@ var config = {
             description: 'Copy these sections to add to your story.',
             location: {
                 center: [-59.19000, -5.93000],
-                zoom: 4.11,
-                pitch: 60,
-                bearing: 0
+                zoom: 4,
+                pitch: 60.00,
+                bearing: 0.00
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
@@ -98,9 +175,9 @@ var config = {
             description: 'Copy these sections to add to your story.',
             location: {
                 center: [-59.19000, -5.93000],
-                zoom: 4.11,
-                pitch: 60,
-                bearing: 0
+                zoom: 4,
+                pitch: 60.00,
+                bearing: 0.00
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
